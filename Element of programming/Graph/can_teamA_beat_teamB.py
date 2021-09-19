91fbdeb312aabf19375b13e68213a85d223f400b
Utkarsh Vijay <utkarshvijay99@gmail.com>
2021-09-19 16:26:39 +0530

    adding coursera files + graph folder

---
+++ Element of programming/Graph/can_teamA_beat_teamB.py
@@ -0,0 +1,36 @@
+import collections
+"""basically this question is of simple dfs the question asks whether,
+     teamA can reach teamB or not?"""
+
+
+def can_teamA_beat_teamB(matches, teamA, teamB):
+    def buildGraph():
+        winning_team = 0
+        losing_team = 1
+        graph = collections.defaultdict(set)
+        for match in matches:
+            graph[match[winning_team]].add(match[losing_team])
+        return graph
+
+    def dfs(graph, start, dest, visited=set()):
+        if start == dest:
+            return True
+        elif start in visited or start not in graph:
+            return False
+        visited.add(start)
+        return any(dfs(graph, team, dest, visited) for team in graph[start])
+
+    def bfs(graph, start, dest, visited=set()):
+        visited.add(start)
+        queue = [start]
+        while queue:
+            curr_team = queue.pop(0)
+            if curr_team == dest:
+                return True
+            for team in graph[curr_team]:
+                if team not in visited:
+                    visited.add(team)
+                    queue.append(team)
+        return False
+
+    return (dfs(buildGraph(), teamA, teamB))
